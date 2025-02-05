616f49c760648a46e4231daffb806e5101f0ee18
Lychtael <jitabilhaq12@gmail.com>
2025-02-05 22:51:11 +0700

    initial commit

---
+++ backend/public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas harum, culpa animi consectetur ducimus, modi et deleniti nulla ratione id omnis commodi debitis nemo reiciendis esse dolores non laborum. Id laborum explicabo, sit et quisquam similique quia quos ex dolores distinctio cumque deleniti autem nihil, vero alias possimus? Praesentium, cupiditate.</h1>
+</body>
+</html>
